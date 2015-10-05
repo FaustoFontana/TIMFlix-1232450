41bb070565c05d8e57f63470038b42815dc75d24
Etudiant(e) Tim <eTim@LD140-M55-02801.local>
2015-10-05 14:46:02 -0400

    Initial Commit

---
+++ TIMFlix-1232450/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TIMFlix-1232450
+//
+//  Created by Etudiant(e) Tim on 2015-10-05.
+//  Copyright (c) 2015 timStudent. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
